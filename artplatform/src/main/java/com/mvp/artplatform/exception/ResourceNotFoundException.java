bdc93e2f9112a27d597348454a7ec75a61ac09bd
Jordan Cupid <jordancupid2@gmail.com>
2024-12-06 04:27:01 -0500

    Create custom exception for missing resources

---
+++ artplatform/src/main/java/com/mvp/artplatform/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mvp.artplatform.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    public ResourceNotFoundException(String message, Throwable cause) {super(message, cause);}
+}
