0230a1dbc0fe9ad770d90a6774c85b2bf3f8710a
Jordan Cupid <jordancupid2@gmail.com>
2024-12-06 04:27:52 -0500

    Create custom exception for issues with external APIs

---
+++ artplatform/src/main/java/com/mvp/artplatform/exception/ApiClientException.java
@@ -0,0 +1,8 @@
+package com.mvp.artplatform.exception;
+
+public class ApiClientException extends RuntimeException {
+    public ApiClientException(String message) {
+        super(message);
+    }
+    public ApiClientException(String message, Throwable cause) { super(message, cause); }
+}
