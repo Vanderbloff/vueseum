5d4da458176722eabf194c0fa9e8606dd0be50d6
Jordan Cupid <jordancupid2@gmail.com>
2024-12-20 05:19:45 -0500

    Implement web configuration class to enable automatic Pageable parameter resolution

---
+++ artplatform/src/main/java/com/mvp/artplatform/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.mvp.artplatform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+public class WebConfig implements WebMvcConfigurer {
+    // This enables automatic Pageable parameter resolution
+    // No need for additional complexity yet
+}
