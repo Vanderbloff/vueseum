fb7821351bbb86147e1175a923110f6718bb3948
Jordan Cupid <jordancupid2@gmail.com>
2024-12-26 21:27:00 -0500

    Create base entity model to enforce versioning amongst all entities

--- artplatform/src/main/java/com/mvp/artplatform/entity/base/baseEntity.java
+++ artplatform/src/main/java/com/mvp/artplatform/entity/base/baseEntity.java
@@ -1,15 +1,19 @@
-package com.mvp.artplatform.model.base;
+package com.mvp.artplatform.entity.base;
 
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public abstract class baseEntity {
@@ -28,4 +32,23 @@ public abstract class baseEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    // Add soft delete support
+    @Column(name = "is_deleted")
+    private Boolean deleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void markAsDeleted() {
+        this.deleted = true;
+        this.deletedAt = LocalDateTime.now();
+    }
 }
