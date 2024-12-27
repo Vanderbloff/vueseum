c807fb158075e9d88a0cb726119291ba7bea516c
Jordan Cupid <jordancupid2@gmail.com>
2024-12-26 21:08:28 -0500

    Add custom getter for mapping of museum and museum hours; set null check on rate limit property from properties file to ensure API call rates are respected

--- artplatform/src/main/java/com/mvp/artplatform/config/MuseumConfig.java
+++ artplatform/src/main/java/com/mvp/artplatform/config/MuseumConfig.java
@@ -2,9 +2,9 @@ package com.mvp.artplatform.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mvp.artplatform.model.Museum;
+import com.mvp.artplatform.entity.Museum;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +13,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @Configuration
 @ConfigurationProperties(prefix = "museum")
-@PropertySource("classpath:museums.properties")
+@PropertySource("classpath:museum.properties")
 @Getter
 @Setter
 public class MuseumConfig {
-    private final Environment environment;
+    private Environment environment;
 
     @Autowired
     public MuseumConfig(Environment environment) {
@@ -36,8 +38,14 @@ public class MuseumConfig {
         private String location;
         //private String address;
         private String websiteUrl;
-        private Map<String, Museum.museumHours> museumHours;
+
+        @Getter(AccessLevel.NONE)
+        private Map<String, Museum.museumHours> museumHours = new HashMap<>();
         private int rateLimit;
+
+        public Map<String, Museum.museumHours> getMuseumHours() {
+            return new HashMap<>(museumHours);
+        }
     }
 
     public MuseumProperties setupMuseumConfig(String museumIdentifier) {
@@ -47,7 +55,7 @@ public class MuseumConfig {
         museumProperties.setName(environment.getProperty(basePath + ".name"));
         museumProperties.setLocation(environment.getProperty(basePath + ".location"));
         museumProperties.setWebsiteUrl(environment.getProperty(basePath + ".websiteUrl"));
-        museumProperties.setRateLimit(Integer.parseInt(environment.getProperty(basePath + ".api.rateLimit")));
+        museumProperties.setRateLimit(Integer.parseInt(Objects.requireNonNull(environment.getProperty(basePath + ".api.rateLimit"))));
         String hours = environment.getProperty(basePath + ".hours");
         if (hours != null) {
             try {
@@ -59,7 +67,6 @@ public class MuseumConfig {
                 throw new RuntimeException("Error parsing hours for " + museumIdentifier, e);
             }
         }
-
         return museumProperties;
     }
 }
