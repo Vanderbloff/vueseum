082d4dff3a3fd2827558e69e159280294dc55352
Jordan Cupid <jordancupid2@gmail.com>
2025-01-14 19:31:56 -0500

    Add properties and methods for sorting that will be used in search criteria

--- vueseum/src/main/java/com/mvp/vueseum/dto/ArtworkSearchCriteria.java
+++ vueseum/src/main/java/com/mvp/vueseum/dto/ArtworkSearchCriteria.java
@@ -3,6 +3,7 @@ package com.mvp.vueseum.dto;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -46,4 +47,32 @@ public class ArtworkSearchCriteria {
     private List<String> museums;
 
     private List<String> tags;
+
+    @Pattern(regexp = "^(relevance|title|artist|date)$")
+    @Getter(AccessLevel.NONE)
+    private String sortField;
+
+    @Pattern(regexp = "^(asc|desc)$")
+    @Getter(AccessLevel.NONE)
+    private String sortDirection;
+
+    public Sort.Direction getSortDirection() {
+        if (sortDirection == null) {
+            return Sort.Direction.ASC;
+        }
+        return Sort.Direction.fromString(sortDirection);
+    }
+
+    public String getSortField() {
+        return sortField != null ? sortField : "relevance";
+    }
+
+    public static String mapSortField(String field) {
+        return switch (field) {
+            case "title" -> "title";
+            case "artist" -> "artist.artistName";
+            case "date" -> "creationDate";
+            default -> "id";
+        };
+    }
 }
