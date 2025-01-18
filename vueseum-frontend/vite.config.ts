d4766e16e381b7cf88215e1b6945f08b3940f580
Jordan Cupid <jordancupid2@gmail.com>
2025-01-18 00:39:11 -0500

    Update configuration to include JSON server (for development purposes)

--- vueseum-frontend/vite.config.ts
+++ vueseum-frontend/vite.config.ts
@@ -3,8 +3,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
 	test: {
+		environment: 'jsdom',
+		globals: true,
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3001',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
 	}
 });
