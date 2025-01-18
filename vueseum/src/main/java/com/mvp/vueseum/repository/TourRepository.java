f51fbb6b1e71c21fcc65e23e90b485e710532be9
Jordan Cupid <jordancupid2@gmail.com>
2025-01-17 21:37:26 -0500

    Add interface method for retrieving a paginated tour

--- vueseum/src/main/java/com/mvp/vueseum/repository/TourRepository.java
+++ vueseum/src/main/java/com/mvp/vueseum/repository/TourRepository.java
@@ -1,6 +1,8 @@
 package com.mvp.vueseum.repository;
 
 import com.mvp.vueseum.entity.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ import java.util.Optional;
 public interface TourRepository extends JpaRepository<Tour, Long> {
     Optional<Tour> findByIdAndDeletedFalse(Long id);
     long countByDeviceFingerprintAndDeletedFalse(String deviceFingerprint);
+    Page<Tour> findByDeletedFalse(Pageable pageable);
 }
