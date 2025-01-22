e4f5b7bd0a6c579fbeba898467e6e4788e7c9864
Jordan Cupid <jordancupid2@gmail.com>
2025-01-21 19:28:53 -0500

    Allow filters to be set based off of loaded URL parameters (i.e., enables users to bookmark pages)

--- vueseum-frontend/src/routes/+page.ts
+++ vueseum-frontend/src/routes/+page.ts
@@ -4,8 +4,28 @@ import type { Load } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
 import { ArtworkUtils } from '$lib/utils/artwork/artworkUtils';
 import { tourApi } from '$lib/api/tour';
+import type { StandardPeriod } from '$lib/types/artwork';
 
 export const load: Load = async ({ url }) => {
+	const searchParams = url.searchParams;
+	const initialFilters = {
+		searchTerm: [searchParams.get('q') ?? ''],
+		searchField: (searchParams.get('searchField') ?? 'all') as 'all' | 'title' | 'artist' | 'culture',
+		objectType: searchParams.getAll('objectType'),
+		materials: searchParams.getAll('medium'),
+		country: searchParams.getAll('country'),
+		region: searchParams.getAll('region'),
+		culture: searchParams.getAll('culture'),
+		era: searchParams.getAll('period') as StandardPeriod[],
+		onDisplay: searchParams.get('onDisplay') === 'true',
+		hasImage: true
+	};
+
+	const sort = {
+		field: (searchParams.get('sortBy') ?? 'relevance') as 'relevance' | 'title' | 'artist' | 'date',
+		direction: (searchParams.get('sortDirection') ?? 'asc') as 'asc' | 'desc'
+	};
+
 	const artworkPage = Number(url.searchParams.get('artworkPage')) || 0;
 	const tourPage = Number(url.searchParams.get('tourPage')) || 0;
 	const initialTab = url.searchParams.get('tab') || 'artworks';
@@ -18,7 +38,9 @@ export const load: Load = async ({ url }) => {
 			)
 			: null;
 
-		const toursPromise = tourApi.getTours(tourPage);
+		const toursPromise = initialTab === 'tours' || initialTab === null
+			? await tourApi.getTours(tourPage)
+			: null;
 
 		const [artworks, tours] = await Promise.all([
 			artworksPromise,
@@ -28,7 +50,9 @@ export const load: Load = async ({ url }) => {
 		return {
 			artworks,
 			tours,
-			initialTab
+			initialTab,
+			initialFilters,
+			initialSort: sort
 		};
 	} catch (e) {
 		console.error('Error loading initial data:', e);
