413113380d7888cb92ff1f57f8e1e7909a61135d
Jordan Cupid <jordancupid2@gmail.com>
2025-01-26 07:15:58 -0500

    Remove unused repository methods; add new method that counts each museum's artworks

--- vueseum/src/main/java/com/mvp/vueseum/repository/ArtworkRepository.java
+++ vueseum/src/main/java/com/mvp/vueseum/repository/ArtworkRepository.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,13 +25,14 @@ public interface ArtworkRepository extends JpaRepository<Artwork, Long>, JpaSpec
     @Query("SELECT DISTINCT a.culture FROM Artwork a WHERE a.culture IS NOT NULL")
     List<String> findDistinctCultures();
 
-    Optional<Artwork> findByExternalId(String externalId);
     Optional<Artwork> findByExternalIdAndMuseum(String externalId, Museum museum);
     @NotNull Page<Artwork> findAll(Specification<Artwork> specification, @NotNull Pageable pageable);
-    Page<Artwork> findByDisplayStatusCheckBefore(LocalDateTime threshold, Pageable pageable);
 
     @Query("SELECT a FROM Artwork a " +
             "LEFT JOIN FETCH a.artist " +
             "LEFT JOIN FETCH a.museum")
     List<Artwork> findAllWithArtistsAndMuseums();
+
+    @Query("SELECT COUNT(a) FROM Artwork a WHERE a.museum.id = :museumId")
+    long countByMuseum(Long museumId);
 }
