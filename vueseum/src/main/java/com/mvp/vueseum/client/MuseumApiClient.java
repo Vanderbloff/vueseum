27f496c581781c84bac864c75aafb142f72b766b
Jordan Cupid <jordancupid2@gmail.com>
2025-01-27 20:33:38 -0500

    Add methods to interface that will be required by all museum client implementations

--- vueseum/src/main/java/com/mvp/vueseum/client/MuseumApiClient.java
+++ vueseum/src/main/java/com/mvp/vueseum/client/MuseumApiClient.java
@@ -1,8 +1,19 @@
 package com.mvp.vueseum.client;
 
 import com.mvp.vueseum.domain.ArtworkDetails;
+import com.mvp.vueseum.event.SyncOperation;
+import com.mvp.vueseum.exception.ApiClientException;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MuseumApiClient {
+    List<String> getCurrentlyDisplayedArtworkIds() throws ApiClientException;
     ArtworkDetails fetchArtworkById(String Id);
-    void syncArtworks();
+    int getErrorCount();
+    LocalDateTime getSyncStartTime();
+    int getProcessedCount();
+    Long getMuseumId();
+
+    void performSync(SyncOperation operation);
 }
