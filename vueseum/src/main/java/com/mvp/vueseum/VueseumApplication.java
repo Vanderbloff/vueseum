546abce3a1d77beb6c2a1081e8db6c4583e29f83
Jordan Cupid <jordancupid2@gmail.com>
2025-02-03 19:39:32 -0500

    Update import configuration to ensure all custom .properties files are scannable

--- vueseum/src/main/java/com/mvp/vueseum/VueseumApplication.java
+++ vueseum/src/main/java/com/mvp/vueseum/VueseumApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mvp.vueseum")
 @EnableScheduling
 public class VueseumApplication {
 	public static void main(String[] args) {
