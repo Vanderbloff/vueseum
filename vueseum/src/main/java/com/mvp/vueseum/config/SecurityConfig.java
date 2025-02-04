540b1583220004e18f3dca704eeed94a8bc66c5c
Jordan Cupid <jordancupid2@gmail.com>
2025-02-03 20:49:05 -0500

    Bind CorsProperties to SecurityConfig with @EnableConfigurationProperties, to register the properties class as a Spring bean

--- vueseum/src/main/java/com/mvp/vueseum/config/SecurityConfig.java
+++ vueseum/src/main/java/com/mvp/vueseum/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mvp.vueseum.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +24,7 @@ import java.util.Arrays;
  */
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(CorsProperties.class)
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final CorsProperties corsProperties;
