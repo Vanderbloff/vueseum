870b8b9ab1945e3963836ce676fdf2c063794944
Jordan Cupid <jordancupid2@gmail.com>
2025-02-12 23:23:17 -0500

    Create entry-point script that determines which commands to run to build the frontend based on the environment (used by Dockerfile)

---
+++ vueseum-frontend/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$NODE_ENV" = "development" ]; then
+    echo "Starting in development mode..."
+    exec npm run dev -- --host 0.0.0.0
+else
+    echo "Starting in production mode..."
+    npm run build
+    exec "$@"
+fi
