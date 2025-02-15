48fdba07073d3fab6d22656c92f5a6185bf726c8
Jordan Cupid <jordancupid2@gmail.com>
2025-02-14 23:46:00 -0500

    Update configuration of node adapter

--- vueseum-frontend/svelte.config.js
+++ vueseum-frontend/svelte.config.js
@@ -4,9 +4,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter()
+	},
+	files: {
+		lib: 'src/lib',
+		routes: 'src/routes',
+		appTemplate: 'src/app.html'
 	}
 };
 
