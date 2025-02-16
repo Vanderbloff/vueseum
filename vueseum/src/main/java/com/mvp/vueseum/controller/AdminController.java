5ea8641acfdc0ed99024a7f52a5a779459732eaa
Jordan Cupid <jordancupid2@gmail.com>
2025-02-16 08:07:28 -0500

    Use Spring Security to restrict access to endpoints

--- vueseum/src/main/java/com/mvp/vueseum/controller/AdminController.java
+++ vueseum/src/main/java/com/mvp/vueseum/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.mvp.vueseum.event.SyncOperation;
 import com.mvp.vueseum.service.SyncManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -11,6 +12,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
     private final SyncManagementService syncService;
 
