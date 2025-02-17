7f17e9679c7f9282567278e53be8e500a39fe0f0
Jordan Cupid <jordancupid2@gmail.com>
2025-02-17 03:30:49 -0500

    Enable method security to whole application context

--- vueseum/src/main/java/com/mvp/vueseum/VueseumApplication.java
+++ vueseum/src/main/java/com/mvp/vueseum/VueseumApplication.java
@@ -3,9 +3,11 @@ package com.mvp.vueseum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication(scanBasePackages = "com.mvp.vueseum")
 @EnableScheduling
+@EnableMethodSecurity
 public class VueseumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VueseumApplication.class, args);
