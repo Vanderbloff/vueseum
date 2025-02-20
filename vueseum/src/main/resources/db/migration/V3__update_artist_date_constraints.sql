7058440654b2ec13775b0cf2a4d207e6e171d8a9
Jordan Cupid <jordancupid2@gmail.com>
2025-02-20 11:57:57 -0500

    Rollback changes due to migration checksum mismatch (need to create V4)

--- vueseum/src/main/resources/db/migration/V3__update_artist_date_constraints.sql
+++ vueseum/src/main/resources/db/migration/V3__update_artist_date_constraints.sql
@@ -1,21 +1,8 @@
-ALTER TABLE artists
-    DROP CONSTRAINT birth_date_format,
-    DROP CONSTRAINT death_date_format,
-    DROP CONSTRAINT valid_lifespan;
+ALTER TABLE artists DROP CONSTRAINT birth_date_format;
+ALTER TABLE artists DROP CONSTRAINT death_date_format;
 
 -- Allow either empty string or 4 digits
-ALTER TABLE artists
-    ADD CONSTRAINT birth_date_format
-        CHECK (birth_date IS NULL OR birth_date = '' OR birth_date ~ '^[0-9]{4}$'),
-    ADD CONSTRAINT death_date_format
-        CHECK (death_date IS NULL OR death_date = '' OR death_date ~ '^[0-9]{4}$'),
-    ADD CONSTRAINT valid_lifespan
-        CHECK (
-            birth_date IS NULL OR birth_date = '' OR
-            death_date IS NULL OR death_date = '' OR
-            (
-                birth_date ~ '^[0-9]{4}$' AND
-                death_date ~ '^[0-9]{4}$' AND
-                CAST(death_date AS INTEGER) - CAST(birth_date AS INTEGER) BETWEEN 1 AND 120
-            )
-        );
+ALTER TABLE artists ADD CONSTRAINT birth_date_format
+    CHECK (birth_date IS NULL OR birth_date = '' OR birth_date ~ '^[0-9]{4}$');
+ALTER TABLE artists ADD CONSTRAINT death_date_format
+    CHECK (death_date IS NULL OR death_date = '' OR death_date ~ '^[0-9]{4}$');
