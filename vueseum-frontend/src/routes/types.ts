cd49037eccdb0249c41804bfc2c3319c595955f7
Jordan Cupid <jordancupid2@gmail.com>
2025-02-23 19:52:03 -0500

    Create new type that returns all possible page data for application, including artworks, tours, and filters and filter options

---
+++ vueseum-frontend/src/routes/types.ts
@@ -0,0 +1,26 @@
+import type { PaginatedResponse, Artwork, StandardPeriod } from '$lib/types/artwork';
+import type { FilterOptions } from '$lib/api/artwork';
+import type { Tour } from '$lib/types/tour';
+
+export interface PageData {
+	artworks: PaginatedResponse<Artwork>;
+	tours: PaginatedResponse<Tour>;
+	initialTab: string;
+	initialFilters: {
+		searchTerm: string[];
+		searchField: 'all' | 'title' | 'artist' | 'culture';
+		objectType: string[];
+		materials: string[];
+		country: string[];
+		region: string[];
+		culture: string[];
+		era: StandardPeriod[];
+		hasImage: boolean;
+		museumId: string[];
+	};
+	initialSort: {
+		field: string;
+		direction: 'asc' | 'desc';
+	};
+	filterOptions: FilterOptions;
+}
