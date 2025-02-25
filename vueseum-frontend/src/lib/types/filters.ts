53356c52c5b603f28a195b8dff6b1348e3f484de
Jordan Cupid <jordancupid2@gmail.com>
2025-02-24 23:26:50 -0500

    Move ArtworkFilters interface and SearchField type from ArtworkFilters.svelte to centralize logic; implement FilterChangeHandler type to be used for onFilterChange prop

--- vueseum-frontend/src/lib/types/filters.ts
+++ vueseum-frontend/src/lib/types/filters.ts
@@ -1,4 +1,25 @@
-// src/lib/types/filters.ts
+import type { StandardPeriod } from './artwork';
+
+export type SearchField = 'all' | 'title' | 'artist' | 'culture';
+
+export interface ArtworkFilters {
+	searchTerm: string[];
+	searchField: SearchField;
+	objectType: string[];
+	materials: string[];
+	country: string[];
+	region: string[];
+	culture: string[];
+	era: StandardPeriod[];
+	hasImage: boolean;
+	museumId: string[];
+}
+
+export type FilterChangeHandler = (
+	key: string,
+	value: string[] | boolean | StandardPeriod[] | SearchField
+) => void;
+
 export interface FilterOptionsResponse {
 	objectType: string[];
 	mediums: string[];
