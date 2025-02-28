31622322b9b6768ffbcb432228bb8b349fe9eabf
Jordan Cupid <jordancupid2@gmail.com>
2025-02-28 17:19:07 -0500

    Modify controller to return proper JSON in response for endpoint

--- vueseum/src/main/java/com/mvp/vueseum/controller/DeviceFingerprintController.java
+++ vueseum/src/main/java/com/mvp/vueseum/controller/DeviceFingerprintController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/device")
@@ -15,7 +17,8 @@ public class DeviceFingerprintController {
     private final DeviceFingerprintService fingerprintService;
 
     @GetMapping("/fingerprint")
-    public ResponseEntity<String> getFingerprint(HttpServletRequest request) {
-        return ResponseEntity.ok(fingerprintService.generateFingerprint(request));
+    public ResponseEntity<Map<String, String>> getFingerprint(HttpServletRequest request) {
+        String fingerprint = fingerprintService.generateFingerprint(request);
+        return ResponseEntity.ok(Map.of("fingerprint", fingerprint));
     }
 }
