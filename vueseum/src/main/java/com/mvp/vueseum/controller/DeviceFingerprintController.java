e7fce33f0510b02abbb4af9dd803d65ee7242dda
Jordan Cupid <jordancupid2@gmail.com>
2025-03-01 04:06:49 -0500

    Modify endpoint method to include logging to trace where fingerprint was requested from and use new functionality in DeviceFingerprintService

--- vueseum/src/main/java/com/mvp/vueseum/controller/DeviceFingerprintController.java
+++ vueseum/src/main/java/com/mvp/vueseum/controller/DeviceFingerprintController.java
@@ -2,7 +2,9 @@ package com.mvp.vueseum.controller;
 
 import com.mvp.vueseum.service.visitor.DeviceFingerprintService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,19 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/device")
+@Slf4j
 public class DeviceFingerprintController {
     private final DeviceFingerprintService fingerprintService;
 
     @GetMapping("/fingerprint")
-    public ResponseEntity<Map<String, String>> getFingerprint(HttpServletRequest request) {
-        String fingerprint = fingerprintService.generateFingerprint(request);
+    public ResponseEntity<Map<String, String>> getFingerprint(
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        log.debug("Fingerprint requested from IP: {}", request.getRemoteAddr());
+
+        String fingerprint = fingerprintService.getOrCreateFingerprint(request, response);
+        log.debug("Returning fingerprint: {}", fingerprint);
+
         return ResponseEntity.ok(Map.of("fingerprint", fingerprint));
     }
 }
