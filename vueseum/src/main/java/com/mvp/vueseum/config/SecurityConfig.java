632e5d2e982cd728b1473cced15443de2fe07cbb
Jordan Cupid <jordancupid2@gmail.com>
2025-03-03 17:59:20 -0500

    Disable CSRF protection for now due to Spring Security constraints; device fingerprinting will provide sufficient, basic security for time being

--- vueseum/src/main/java/com/mvp/vueseum/config/SecurityConfig.java
+++ vueseum/src/main/java/com/mvp/vueseum/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,10 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -51,7 +50,8 @@ public class SecurityConfig {
                 )
 
                 // CSRF protection configuration
-                .csrf(csrf -> csrf
+                .csrf(AbstractHttpConfigurer::disable)
+                /*.csrf(csrf -> csrf
                         .ignoringRequestMatchers(
                                 new AntPathRequestMatcher("/api/v1/public/**"),
                                 new AntPathRequestMatcher("/api/v1/admin/**"),
@@ -60,7 +60,7 @@ public class SecurityConfig {
                                 request -> request.getMethod().equals("OPTIONS")
                         )
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
+                )*/
 
                 // Configure which URLs are accessible
                 .authorizeHttpRequests(auth -> auth
